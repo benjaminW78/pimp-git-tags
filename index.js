20af6cdf1b08d0cbd2ee92daea73bd22d89c952a
bwinckell <bwinckell@companeo.com>
2017-11-28 17:37:21 +0100

    add project code

---
+++ index.js
@@ -0,0 +1,91 @@
+const gitTags = require('git-tags');
+const semver = require('semver');
+const inquirer = require('inquirer');
+const child_process = require('child_process');
+const cmd = require('node-cmd');
+
+(async function(){
+
+    let lastTag = await new Promise((resolve, reject) => {
+        gitTags.latest((err, lastTag) => {
+            if(err){
+                reject(err);
+            }else{
+                resolve(lastTag);
+            }
+        });
+    });
+
+    console.log('|---------------------------------------------------------|');
+    console.log('|  Welcome to Pimp my tag                                 |');
+    console.log('|  version 1.0                                            |');
+    console.log('|  based on NodeJS 8.6 and developped by bwinckell        |');
+    console.log('|---------------------------------------------------------|');
+
+    console.log('|---------------------------------------------------------|');
+    console.log('|  Last tag was : ',lastTag,'                               |');
+    console.log('|  Next possibilities:                                    |');
+    console.log('|     - Major version:', semver.inc(lastTag,'major'),'                            |');
+    console.log('|     - Minor version:', semver.inc(lastTag,'minor'),'                            |');
+    console.log('|     - Patch version:', semver.inc(lastTag,'patch'),'                            |');
+    console.log('|---------------------------------------------------------|');
+
+    console.log('\n');
+
+    let t = await inquirer.prompt([{type:'list',name:'bumpType',message:'Select the type of bump you want to proceed',default:'minor',pageSize:10,choices:[new inquirer.Separator(),'major',new inquirer.Separator(),'minor',new inquirer.Separator(),'patch',new inquirer.Separator(),'exit program'],prefix:'Bump tag version \n'}])
+    if(t.bumpType!='exit program'){
+
+    let newTag = 'v'+semver.inc(lastTag,t.bumpType);
+
+    let choice = await inquirer.prompt([{type:'confirm',name:'value',message:'Do you want to add '+newTag+' to the current git project and at current HEAD position',prefix:'Generate new git tag \n'}])
+    if(choice.value){
+        generateNewGitTag(newTag);
+        console.log('you created a new tag => '+ newTag);
+        console.log('Don\'t forget to push your tag to share it with \'git push origin --tags\'');
+    }else{
+        let choice1 = await inquirer.prompt([{type:'confirm',name:'value',message:'Do you want to add '+newTag+' to a specific commit number',prefix:'Generate new git tag custom\n'}])
+        if(choice1.value){
+            let commit = await inquirer.prompt([{type:'input',name:'value',message:'Please provide a commit hash currently existing on your computer ==> : ',prefix:'Generate new git tag custom\n'}])
+            generateNewGitTag(newTag,commit.value);
+            console.log('you created a new tag => '+ newTag);
+            console.log('Don\'t forget to push your tag to share it with \'git push origin --tags\'');
+        }else {
+            console.log('\n see you soon budy');
+        }
+    }
+    }else{
+        console.log('\n see you soon budy');
+    }
+
+})();
+
+async function generateNewGitTag(newTag,commit=false){
+    await newTagMessage();
+    let editor = 'git';
+    if(commit===false){
+        let child = child_process.spawn(editor,['tag',newTag,'-a','-F','/tmp/temp.txt'], {
+            stdio: 'inherit'
+        });
+    }else{
+        let child = child_process.spawn(editor,['tag',newTag,commit,'-a','-F','/tmp/temp.txt'], {
+            stdio: 'inherit'
+        });
+    }
+};
+function newTagMessage(){
+    return new Promise(function(resolve,reject){
+
+    let editor = 'nano';
+    cmd.get('cp  -f templateTag.txt /tmp/temp.txt');
+    let child = child_process.spawn(editor,['/tmp/temp.txt'], {
+        stdio: 'inherit'
+    });
+    child.on('exit', function (e, code) {
+        if(e){
+            reject(e);
+        }else{
+            resolve(true);
+        }
+    });
+    })
+}
