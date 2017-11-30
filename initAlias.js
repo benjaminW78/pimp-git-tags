86c540d115e513e5c71b3ec2ae7556e2cbce648e
bwinckell <bwinckell@companeo.com>
2017-11-30 14:25:46 +0100

    add init cmd to run a project installation

---
+++ initAlias.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+const pathBash = require('os').homedir();
+const alias = '\nalias pimpGitTag=\' npm --prefix '+ process.env.PWD +' run start "$PWD"\'';
+
+if(fs.existsSync(pathBash+'/.bashrc.d/bashrc_aliases')){
+    appendLineToFile(alias,pathBash+'/.bashrc.d/bashrc_aliases');
+}else if(fs.existsSync(pathBash+'/.bashrc')){
+    appendLineToFile(alias,pathBash+'/.bashrc');
+}
+
+function appendLineToFile(content,filePath){
+    fs.appendFile(filePath,content,'utf8',function (err) {
+        if (err){
+            console.log('une erreur c\'est produite\n',err);
+        }else{
+            console.log('Ajout de l\'alias pimpGitTag dans : '+ filePath +' \n');
+        }
+    });
+}
